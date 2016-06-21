70cf60e5dc59c476d76793517205e2a83b2b6c3c
lujianrong <lujianrong_ios@126.com>
2016-06-21 10:00:25 +0800

    添加发送消息类

---
+++ LJRRuntime/Classes/MessageSend/LJRRTMessage.h
@@ -0,0 +1,13 @@
+//
+//  LJRRTMessage.h
+//  LJRRuntime
+//
+//  Created by lujianrong on 16/6/21.
+//  Copyright © 2016年 LJR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LJRRTMessage : NSObject
+
+@end
---
+++ LJRRuntime/Classes/MessageSend/LJRRTMessage.m
@@ -0,0 +1,13 @@
+//
+//  LJRRTMessage.m
+//  LJRRuntime
+//
+//  Created by lujianrong on 16/6/21.
+//  Copyright © 2016年 LJR. All rights reserved.
+//
+
+#import "LJRRTMessage.h"
+
+@implementation LJRRTMessage
+
+@end
