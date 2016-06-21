c0a45cfaabf1d58d0883f9fb886d164e36368479
lujianrong <lujianrong_ios@126.com>
2016-06-21 15:28:45 +0800

    添加C 语言版方法交换

---
+++ LJRRuntime/Classes/MethodExchange/LJRMethodExchange.h
@@ -0,0 +1,13 @@
+//
+//  LJRMethodExchange.h
+//  LJRRuntime
+//
+//  Created by lujianrong on 16/6/21.
+//  Copyright © 2016年 LJR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LJRMethodExchange : NSObject
++ (void)methodExchangeTest;
+@end
---
+++ LJRRuntime/Classes/MethodExchange/LJRMethodExchange.m
@@ -0,0 +1,15 @@
+//
+//  LJRMethodExchange.m
+//  LJRRuntime
+//
+//  Created by lujianrong on 16/6/21.
+//  Copyright © 2016年 LJR. All rights reserved.
+//
+
+#import "LJRMethodExchange.h"
+
+@implementation LJRMethodExchange
++ (void)methodExchangeTest {
+    
+}
+@end
